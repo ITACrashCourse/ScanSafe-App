4ebaddc63d6958bbc5409fd9ba3b2c9aee4238b2
Camillus83 <kamilrichter98@gmail.com>
2023-02-02 10:02:21 +0100

    Reformat code:config.py

--- flask_project/config.py
+++ flask_project/config.py
@@ -1,11 +1,15 @@
 """config.py - """
+
+
 class Config(object):
     DEBUG = False
     TESTING = False
 
+
 class DevelopmentConfig(Config):
     DEBUG = True
-    IPQS_SECRET_KEY = '' # Put here your ip quality score secret key.
+    IPQS_SECRET_KEY = ""  # Put here your ip quality score secret key.
+
 
 class TestingConfig(Config):
     TESTING = True
