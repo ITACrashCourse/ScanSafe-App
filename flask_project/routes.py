6f505b551bf4c2b71dbd1b4ce27a842c8134ba99
JakubRad <kubaradziwoniuk@gmail.com>
2023-02-05 20:10:44 +0100

    simple endpoint iplemented

--- flask_project/routes.py
+++ flask_project/routes.py
@@ -1,6 +1,15 @@
 from flask_project import app
-from flask import render_template
+from flask_project.url_scanner import IPQS, get_domain
+from flask import render_template, request
 
 @app.route('/')
 def home_page():
     return render_template('base.html')
+
+# Scan URL via IP Quality Score service.
+@app.route("/scan-url", methods=["POST"])
+def scan_url():
+    url = request.args.get('url')
+    domain = get_domain(url)
+    result = IPQS().malicious_url_scanner_api(domain)
+    return result
